8faf490b5b3b65f96eaa4b2318c9a2f7fecdf874
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-05-06 01:21:24 +0000

    task 5

---
+++ web_dynamic/4-hbnb.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from flask import Flask, render_template
+import uuid
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/4-hbnb/')
+def filter_hbnb(the_id=None):
+    """ HBNB is filter"""
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    cache_id = (str(uuid.uuid4()))
+    return render_template('4-hbnb.html',
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users,
+                           cache_id=cache_id)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
